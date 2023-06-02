a7f6d4d0667b6e800ccf8c74e747506f08c5edc4
akayo06 <pty106@gmail.com>
2023-06-02 16:48:05 +0800

    add navigation

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,24 +1,61 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { NativeBaseProvider, Box } from "native-base";
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View } from "react-native";
+// import React from "react";
+// import { NativeBaseProvider, Box } from "native-base";
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <NativeBaseProvider>
+//       <View style={styles.container}>
+//         <StatusBar style="auto" />
+//         <Box>Hello world</Box>
+//       </View>
+//     </NativeBaseProvider>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { HomeScreen } from "./src/pages/Demo";
+import { PlansPage } from "./src/pages/PlansPage";
+
+function SettingsScreen() {
   return (
-    <NativeBaseProvider>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Box>Hello world</Box>
-      </View>
-    </NativeBaseProvider>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Meal" component={SettingsScreen} />
+      <Tab.Screen name="Calendar" component={SettingsScreen} />
+      <Tab.Screen name="Plan" component={PlansPage} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
