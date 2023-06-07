cf8247980775f02e0d811e48732f916590dcba35
Henry <yhnganaa@gmail.com>
2023-06-07 23:39:26 +0800

    redirected meal page

--- my-app/src/screens/MealPage.tsx
+++ my-app/src/screens/MealPage.tsx
@@ -1,13 +1,40 @@
-import React, { useState } from "react";
-import { Text, View, Button } from "react-native";
+// Buffer Line
+import { Fragment, useState, useMemo, useEffect } from "react";
+import { Provider } from "react-redux";
+import { store } from "../store";
+import DateSelectionPanel from "../components/mealPageComponents/DateSelectionPanel";
+import MealTypeSelection from "../components/mealPageComponents/MealTypeSelection";
+import { FullItemInfo } from "../utils/type";
+import { fakeFoodNutritionData } from "../components/mealPageComponents/fakeFoodNutritionData";
+
+const MealPage: React.FC = () => {
+  const [date, updateSelectedDate] = useState<Date>(new Date());
+  const [dateMealData, updateDateMealData] = useState<FullItemInfo[]>([]);
+
+  const getDateMealData = async (date: Date) => {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    return fakeFoodNutritionData;
+  };
+
+  const updateMealData = async () => {
+    let newData = await getDateMealData(date);
+    updateDateMealData(newData);
+  };
+
+  useMemo(() => {
+    updateMealData();
+  }, [date]);
+
+  return (
+    <Fragment>
+      <Provider store={store}>
+        {/* <DateSelectionPanel date={date} selectNewDate={updateSelectedDate} /> */}
+        <MealTypeSelection foodItemFullInfo={dateMealData}></MealTypeSelection>
+      </Provider>
+    </Fragment>
+  );
+};
 
 export function MealScreen({}) {
-  return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Meal!</Text>
-      <View>
-        <Text className="text-red-200">2</Text>
-      </View>
-    </View>
-  );
+  return <MealPage />;
 }
