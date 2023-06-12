c626698af064226d55183aa5cabd86126d62097c
akayo06 <pty106@gmail.com>
2023-06-12 20:37:19 +0800

    set up mealplan api

--- my-app/src/screens/PlansDetailPage.tsx
+++ my-app/src/screens/PlansDetailPage.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import { Button, Center, NativeBaseProvider, ScrollView } from "native-base";
+import { StyleSheet, View } from "react-native";
+import {
+  Text,
+  Button,
+  Center,
+  NativeBaseProvider,
+  ScrollView,
+} from "native-base";
 import { WorkoutDetailItem } from "../components/plansPageComponents/workoutDetail";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Domain } from "@env";
@@ -9,15 +15,33 @@ import { Table, Row, Rows } from "react-native-table-component";
 export const PlanDetailScreen = ({ route }: any) => {
   const { id, type } = route.params;
   const [workoutData, setWorkoutData] = useState<any[]>([]);
+  const [mealplanData, setMealplanData] = useState<any[]>([]);
   useEffect(() => {
     const fetchWorkoutData = async () => {
-      console.log("123");
       let res = await fetch(`${Domain}/workout/detail/${id}`);
       let workout_data = await res.json();
       setWorkoutData(workout_data);
     };
     fetchWorkoutData();
   }, []);
+  useEffect(() => {
+    const fetchMealPlanData = async () => {
+      let res = await fetch(`${Domain}/meal/detail/${id}`);
+      let mealplan_data = await res.json();
+      setMealplanData(mealplan_data);
+    };
+    fetchMealPlanData();
+  }, []);
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 16,
+      paddingTop: 30,
+      backgroundColor: "#fff",
+    },
+    head: { height: 40, backgroundColor: "#f1f8ff" },
+    text: { textAlign: "center" },
+  });
   return (
     // <View
     //   style={{
@@ -39,16 +63,17 @@ export const PlanDetailScreen = ({ route }: any) => {
 
           {workoutData.map((workout) => (
             <>
-              <Text>{workout.title}</Text>
+              <Text fontSize={"xl"}>{workout.title}</Text>
 
               <Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
                 <Row
                   data={workout.headers}
-                  // style={styles.head}
-                  // textStyle={styles.text}
+                  style={styles.head}
+                  textStyle={styles.text}
                 />
                 <Rows data={workout.rows} />
               </Table>
+              <Text></Text>
             </>
           ))}
         </ScrollView>
